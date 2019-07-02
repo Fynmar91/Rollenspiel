25c1dec7439435d2ff6de2580fdb0a3592747fda
Yannik Seitz <yannikseitz@outlook.de>
2019-07-02 21:06:53 +0200

    First

---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Character : MonoBehaviour
+{
+	[SerializeField]
+	private float speed = 5;
+	protected Vector2 direction;
+	private Rigidbody2D myRigidbody;
+	protected Animator myAnimator;
+	protected bool isAttacking = false;
+	protected Coroutine attackRoutine;
+	public bool isMoving
+	{
+		get
+		{
+			return direction.x != 0 || direction.y != 0;
+		}
+	}
+
+	// Start is called before the first frame update
+	protected virtual void Start()
+	{
+		myRigidbody = GetComponent<Rigidbody2D>();
+		myAnimator = GetComponent<Animator>();
+	}
+
+	// Update is called once per frame
+	protected virtual void Update()
+	{
+		HandleLayers();
+	}
+
+	private void FixedUpdate()
+	{
+		Move();
+	}
+
+	public void Move()
+	{
+		myRigidbody.velocity = direction.normalized * speed;
+	}
+
+	public void HandleLayers()
+	{
+		if (isMoving)
+		{
+			StopAttack();
+			ActivateLayer("WalkLayer");
+			myAnimator.SetFloat("x", direction.x);
+			myAnimator.SetFloat("y", direction.y);
+		}
+		else if (isAttacking)
+		{
+			ActivateLayer("AttackLayer");
+		}	
+		else
+		{
+			ActivateLayer("IdleLayer");
+		}
+	}
+
+	public void ActivateLayer(string layerName)
+	{
+		for (int i = 0; i < myAnimator.layerCount; i++)
+		{
+			myAnimator.SetLayerWeight(i, 0);
+		}
+		myAnimator.SetLayerWeight(myAnimator.GetLayerIndex(layerName), 1);
+	}
+
+	public void StopAttack()
+	{
+		if (isAttacking)
+		{
+			StopCoroutine(attackRoutine);
+			Debug.Log("Attack stopped");
+			isAttacking = false;
+			myAnimator.SetBool("attack", isAttacking);
+		}		
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Character
+{
+	[SerializeField]
+	private Stat health;
+	[SerializeField]
+	private Stat mana;
+	private float initHealth = 100f;
+	private float initMana = 100f;
+
+	// Start is called before the first frame update
+	protected override void Start()
+	{
+		health.Initialize(initHealth, initHealth);
+		mana.Initialize(initMana, initMana);
+		base.Start();
+	}
+
+	// Update is called once per frame
+	protected override void Update()
+	{
+		GetInput();
+		base.Update();
+	}
+
+	private void GetInput()
+	{
+		direction = Vector2.zero;
+
+		// DEBUG
+		if (Input.GetKeyDown(KeyCode.I))
+		{
+			health.MyCurrentValue -= 8;
+			mana.MyCurrentValue -= 4;
+		}
+		if (Input.GetKeyDown(KeyCode.O))
+		{
+			health.MyCurrentValue += 10;
+			mana.MyCurrentValue += 5;
+		}
+		// DEBUG
+
+		if (Input.GetKey(KeyCode.W))
+		{
+			direction += Vector2.up;
+		}
+		if (Input.GetKey(KeyCode.A))
+		{
+			direction += Vector2.left;
+		}
+		if (Input.GetKey(KeyCode.S))
+		{
+			direction += Vector2.down;
+		}
+		if (Input.GetKey(KeyCode.D))
+		{
+			direction += Vector2.right;
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			attackRoutine = StartCoroutine(Attack());
+		}
+	}
+
+	private IEnumerator Attack()
+	{
+		if (!isAttacking && !isMoving)
+		{
+			isAttacking = true;
+			myAnimator.SetBool("attack", isAttacking);
+			yield return new WaitForSeconds(3);
+			Debug.Log("Attack done");
+			StopAttack();
+		}
+	}
+}
