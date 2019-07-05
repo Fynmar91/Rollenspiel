b8aa81226500b9bd93adbdfa292c3fd201c24262
Yannik Seitz <yannikseitz@outlook.de>
2019-07-05 10:18:33 +0200

    Adding Water

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Random = UnityEngine.Random;
+
+public class LevelManager : MonoBehaviour
+{
+	[SerializeField]
+	private Transform map;
+
+	[SerializeField]
+	private Texture2D[] mapData;
+
+	[SerializeField]
+	private MapElement[] mapElements;
+
+	[SerializeField]
+	private Sprite defaultTile;
+
+	private Dictionary<Point, GameObject> waterTiles = new Dictionary<Point, GameObject>();
+
+	private Vector3 WorldStartPos
+	{
+		get
+		{
+			return Camera.main.ScreenToWorldPoint(new Vector3(0, 0));
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		GenerateMap();
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	private void GenerateMap()
+	{
+		int height = mapData[0].height;
+		int width = mapData[0].width;
+
+		for (int i = 0; i < mapData.Length; i++)
+		{
+			for (int x = 0; x < mapData[i].width; x++)
+			{
+				for (int y = 0; y < mapData[i].height; y++)
+				{
+					Color c = mapData[i].GetPixel(x, y);
+
+					MapElement newElement = Array.Find(mapElements, e => e.MyColor == c);
+
+					if (newElement != null)
+					{
+						float xPos = WorldStartPos.x + (defaultTile.bounds.size.x * x);
+						float yPos = WorldStartPos.y + (defaultTile.bounds.size.y * y);
+
+						GameObject go = Instantiate(newElement.MyElementPrefab);
+						go.transform.position = new Vector2(xPos, yPos);
+
+						if (newElement.MyTileTag == "Water")
+						{
+							waterTiles.Add(new Point(x, y), go);
+						}
+
+						if (newElement.MyTileTag == "Tree")
+						{
+							float shade = Random.Range(0.8f, 1.0f);
+							float scale = Random.Range(0.7f, 1.0f);
+							go.GetComponent<SpriteRenderer>().color = new Color(shade, shade, shade, 1);
+							go.transform.localScale = new Vector3(scale, scale, scale);
+							go.GetComponent<SpriteRenderer>().sortingOrder = height*2 - y*2;
+						}
+						go.transform.parent = map;
+					}
+				}
+			}
+		}
+		CheckWater();
+	}
+
+	private void CheckWater()
+	{
+		foreach (KeyValuePair<Point,GameObject> tile in waterTiles)
+		{
+			string composition = TileCheck(tile.Key);
+		}
+	}
+
+	private string TileCheck(Point currentPoint)
+	{
+		string composition = string.Empty;
+
+		for (int x = -1; x <= 1; x++)
+		{
+			for (int y = -1; y <= 1; y++)
+			{
+				if (x != 0 || y != 0)
+				{
+					if (waterTiles.ContainsKey(new Point(currentPoint.MyX + x, currentPoint.MyY + y)))
+					{
+						composition += "W";
+					}
+					else
+					{
+						composition += "G";
+					}
+				}
+			}
+		}
+		Debug.Log(composition);
+
+		return composition;
+	}
+}
+
+[Serializable]
+public class MapElement
+{
+	[SerializeField]
+	private string tileTag;
+
+	[SerializeField]
+	private Color color;
+
+	[SerializeField]
+	private GameObject elementPrefab;
+
+	public Color MyColor { get => color; }
+	public GameObject MyElementPrefab { get => elementPrefab; }
+	public string MyTileTag { get => tileTag; }
+}
+
+public struct Point
+{
+	public int MyX	{ get; set; }
+	public int MyY { get; set; }
+
+	public Point(int x, int y)
+	{
+		this.MyX = x;
+		this.MyY = y;
+	}
+}
---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour, IComparable<Obstacle>
+{
+	private Color defaultColor;
+	private Color fadedColor;
+	private Color currentColor;
+	private float currentAlpha;
+	private Coroutine fadeRoutine;
+
+	public SpriteRenderer MySpriteRenderer { get; set; }
+
+	public int CompareTo(Obstacle other)
+	{
+		if (MySpriteRenderer.sortingOrder > other.MySpriteRenderer.sortingOrder)
+		{
+			return 1;
+		}
+		else if (MySpriteRenderer.sortingOrder < other.MySpriteRenderer.sortingOrder)
+		{
+			return -1;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		MySpriteRenderer = GetComponent<SpriteRenderer>();
+		defaultColor = MySpriteRenderer.color;
+		currentColor = MySpriteRenderer.color;
+		fadedColor = defaultColor;
+		fadedColor.a = 0.7f;
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+
+	public void FadeOut()
+	{
+		if (MySpriteRenderer != null)
+		{
+			currentColor = fadedColor;
+
+			if (fadeRoutine != null)
+			{
+				StopCoroutine(fadeRoutine);
+				fadeRoutine = StartCoroutine(Fade());
+			}
+			else
+			{
+				fadeRoutine = StartCoroutine(Fade());
+			}
+		}
+		
+	}
+
+	public void FadeIn()
+	{
+		currentColor = defaultColor;
+
+		if (fadeRoutine != null)
+		{
+			StopCoroutine(fadeRoutine);
+			fadeRoutine = StartCoroutine(Fade());
+		}
+		else
+		{
+			fadeRoutine = StartCoroutine(Fade());
+		}
+	}
+
+	private IEnumerator Fade()
+	{
+		float progress = 0.0f;
+		float rate = 1.0f / 4;
+
+		while (currentColor.a != MySpriteRenderer.color.a)
+		{
+			progress += rate * Time.deltaTime;
+			currentAlpha = Mathf.Lerp(MySpriteRenderer.color.a, currentColor.a, progress);
+			MySpriteRenderer.color = new Vector4(defaultColor.r, defaultColor.g, defaultColor.b, currentAlpha);
+
+			yield return null;
+		}
+		
+
+	}
+}
