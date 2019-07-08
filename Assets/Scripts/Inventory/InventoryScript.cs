d16cc5b43c2f4c6bf5acac700964fc9158b2064a
Yannik Seitz <yannikseitz@outlook.de>
2019-07-08 15:36:15 +0200

    Inventory scripts

---
+++ Assets/Scripts/Inventory/InventoryScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryScript : MonoBehaviour
+{
+	//DEBUG
+	[SerializeField]
+	private Item[] items;
+	//DEBUG
+
+	private static InventoryScript instance;
+
+	public static InventoryScript MyInstance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				instance = FindObjectOfType<InventoryScript>();
+			}
+			return instance;
+		}
+	}
+
+	private void Awake()
+	{
+		//DEBUG
+		Bag bag = (Bag)Instantiate(items[0]);
+		bag.Initialize(16);
+		bag.Use();
+		//DEBUG
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
+
+// Update is called once per frame
+void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : ScriptableObject
+{
+	[SerializeField]
+	private Sprite icon;
+
+	[SerializeField]
+	private int stackSize;
+
+	private SlotScript slot;
+
+	public Sprite MyIcon { get => icon; }
+	public int MyStackSize { get => stackSize; }
+	protected SlotScript MySlot { get => slot; set => slot = value; }
+}
